e453267001a47b0f8f0406baaa88d0ea981640ea
Master1910 <97669026+Master1910@users.noreply.github.com>
2024-11-26 22:58:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -406,4 +406,4 @@ def leave_chat(data):
 # --- Запуск ---
 if __name__ == '__main__':
     init_db()
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
