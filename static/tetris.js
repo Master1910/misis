0ccaa6390467b4888d63693537127763d6da46a4
Master1910 <97669026+Master1910@users.noreply.github.com>
2024-12-18 21:43:53 +0300

    Update tetris.js

--- static/tetris.js
+++ static/tetris.js
@@ -11,6 +11,31 @@ canvas.height = ROWS * BLOCK_SIZE;
 
 context.scale(BLOCK_SIZE, BLOCK_SIZE);
 
+// Формы фигур
+const shapes = {
+    I: [
+        [[1, 1, 1, 1]]
+    ],
+    J: [
+        [[1, 0, 0], [1, 1, 1]]
+    ],
+    L: [
+        [[0, 0, 1], [1, 1, 1]]
+    ],
+    O: [
+        [[1, 1], [1, 1]]
+    ],
+    S: [
+        [[0, 1, 1], [1, 1, 0]]
+    ],
+    T: [
+        [[0, 1, 0], [1, 1, 1]]
+    ],
+    Z: [
+        [[1, 1, 0], [0, 1, 1]]
+    ]
+};
+
 // Создание сетки
 function createGrid(width, height) {
     const grid = [];
