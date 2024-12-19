498e09ee1c7c01927ff04ad46fc80432d170ab4f
Master1910 <97669026+Master1910@users.noreply.github.com>
2024-12-19 12:06:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -272,5 +272,4 @@ def internal_server_error(e):
 
 # --- Запуск ---
 if __name__ == '__main__':
-    init_db()
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
